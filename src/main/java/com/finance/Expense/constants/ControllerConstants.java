49a7d66bef49aacfdec5b363d0532c5ccb1394e1
ikshvaku <61447188+ikshvaku-codes@users.noreply.github.com>
2025-04-13 17:11:56 +0530

    updating endpoints

--- src/main/java/com/finance/Expense/constants/ControllerConstants.java
+++ src/main/java/com/finance/Expense/constants/ControllerConstants.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class ControllerConstants {
 
-    public final static String BASE_URL = "/api";
-    public final static String EXPENSE_URL = "/expense";
-    public final static String CATEGORY_URL = "/category";
+    public final static String BASE_URL = "/api/v1";
+    public final static String EXPENSE_URL = "/expenses";
+    public final static String CATEGORY_URL = "/categories";
     
    
     public final static String CROSS_ORIGIN = "*";
